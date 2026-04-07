8e6c552dd4a2c29475a4b16bb1668bcf62d33978
Tanmay Jhawar <tanmayjhawar1992@gmail.com>
2026-04-07 17:06:03 +1000

    feat: configure Azure remote state backend in storage account

--- main.tf
+++ main.tf
@@ -4,6 +4,13 @@
 terraform {
   required_version = ">= 1.5.0"
 
+  backend "azurerm" {
+    resource_group_name  = "rg-arcticflow-tfstate"
+    storage_account_name = "satfstatearcticflow"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
+  }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -21,8 +28,8 @@ provider "azurerm" {
 }
 
 provider "snowflake" {
-  account   = var.snowflake_account
-  username  = var.snowflake_user
-  private_key_path       = var.snowflake_private_key_path
-  private_key_passphrase = var.snowflake_private_key_passphrase
+  account                  = var.snowflake_account
+  username                 = var.snowflake_user
+  private_key_path         = var.snowflake_private_key_path
+  private_key_passphrase   = var.snowflake_private_key_passphrase
 }
